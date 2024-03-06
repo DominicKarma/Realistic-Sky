ded098b5531ef5dfd468ca1b76a0fb1331a17294
Dominic <farfetchedking541@gmail.com>
2024-03-05 20:20:10 -0600

    Separate sun drawing

--- Content/RealisticSkyManager.cs
+++ Content/RealisticSkyManager.cs
@@ -48,7 +48,7 @@ namespace RealisticSky.Content
             Vector3 translationDirection = new(1f, Main.BackgroundViewMatrix.Effects.HasFlag(SpriteEffects.FlipVertically) ? -1f : 1f, 1f);
             backgroundMatrix.Translation -= Main.BackgroundViewMatrix.ZoomMatrix.Translation * translationDirection;
 
-            // Draw stars.
+            // Calculate various useful interpolants in advance.
             float worldYInterpolant = Main.LocalPlayer.Center.Y / Main.maxTilesY / 16f;
             float spaceInterpolant = MathHelper.SmoothStep(0f, 1f, Utils.GetLerpValue(0.074f, 0.024f, worldYInterpolant, true));
             float sunriseAndSetInterpolant = Utils.GetLerpValue(0f, 6700f, (float)Main.time, true);
@@ -56,9 +56,11 @@ namespace RealisticSky.Content
                 sunriseAndSetInterpolant *= Utils.GetLerpValue((float)Main.dayLength, (float)Main.dayLength - 6700f, (float)Main.time, true);
             if (!Main.dayTime)
                 sunriseAndSetInterpolant = 0f;
+
+            // Draw stars.
             DrawStars(spaceInterpolant, sunriseAndSetInterpolant, Vector2.Transform(SunPositionSaver.SunPosition, Matrix.Invert(backgroundMatrix)));
 
-            // Prepare for sky drawing.
+            // Prepare for atmosphere drawing by allowing shaders.
             Main.spriteBatch.End();
             Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, backgroundMatrix);
 
@@ -70,7 +72,7 @@ namespace RealisticSky.Content
             {
                 Main.spriteBatch.End();
                 Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.None, Main.Rasterizer, null, Matrix.Identity);
-                DrawSunBloom(spaceInterpolant, 1f - sunriseAndSetInterpolant);
+                SunRenderer.Render(spaceInterpolant, 1f - sunriseAndSetInterpolant);
             }
 
             // Return to standard drawing.
@@ -125,52 +127,6 @@ namespace RealisticSky.Content
                 Main.spriteBatch.Draw(bloom, starDrawPosition, null, starColor * localStarOpacity * 0.9f, 0f, bloom.Size() * 0.5f, starSize / starFlarePulse * 0.2f, 0, 0f);
                 if (starSize >= 0.14f)
                     Main.spriteBatch.Draw(bloom, starDrawPosition, null, brightStarColor * localStarOpacity, 0f, bloom.Size() * 0.5f, starSize / starFlarePulse * 0.1f, 0, 0f);
-            }
-        }
-
-        public static void DrawSunBloom(float spaceInterpolant, float sunriseAndSetInterpolant)
-        {
-            // Make things stronger when in space, and weaker during sunrises and sunsets.
-            float bloomOpacity = MathHelper.Lerp(1f, 0.5f, sunriseAndSetInterpolant);
-            float scaleFactor = (spaceInterpolant * 0.21f + 1f) * bloomOpacity;
-            float pureWhiteInterpolant = MathF.Pow(spaceInterpolant, 2f);
-            Vector2 sunPosition = SunPositionSaver.SunPosition;
-            Texture2D bloom = ModContent.Request<Texture2D>("RealisticSky/Assets/ExtraTextures/BloomCircle").Value;
-
-            // Draw the innermost, bright yellow bloom.
-            Main.spriteBatch.Draw(bloom, sunPosition, null, new Color(1f, 1f, 0.92f, 0f) * bloomOpacity, 0f, bloom.Size() * 0.5f, (MathF.Pow(scaleFactor, 1.5f) + pureWhiteInterpolant * 1.5f) * 0.9f, 0, 0f);
-
-            // Make successive bloom draws weaker in accordance with the white interpolant.
-            bloomOpacity *= MathHelper.Lerp(1f, 0.25f, pureWhiteInterpolant);
-
-            // Draw the mid-bloom. This is still mostly bright, but is biased a bit towards bright maroons during sunrises and sunsets.
-            Color midBloomColor = Color.Lerp(new Color(1f, 1f - sunriseAndSetInterpolant, 0.5f, 0f), Color.White with { A = 0 }, pureWhiteInterpolant);
-            Main.spriteBatch.Draw(bloom, sunPosition, null, midBloomColor * bloomOpacity * 0.48f, 0f, bloom.Size() * 0.5f, scaleFactor * 1.6f, 0, 0f);
-
-            // Draw the outermost bloom. This is mostly red, with a tinge of blue.
-            Color outerBloomColor = Color.Lerp(new Color(1f - pureWhiteInterpolant, 0f, pureWhiteInterpolant * 0.96f + 0.3f, 0f), Color.White with { A = 0 }, pureWhiteInterpolant);
-            Main.spriteBatch.Draw(bloom, sunPosition, null, outerBloomColor * bloomOpacity * 0.3f, 0f, bloom.Size() * 0.5f, scaleFactor * (2.1f - sunriseAndSetInterpolant + pureWhiteInterpolant), 0, 0f);
-
-            // Draw a lens flare over everything when in space.
-            if (spaceInterpolant > 0f)
-            {
-                int lensFlareCount = 1;
-                ulong seed = 11589uL;
-
-                Texture2D lensFlare = ModContent.Request<Texture2D>("RealisticSky/Assets/ExtraTextures/LensFlare").Value;
-                for (int i = 0; i < lensFlareCount; i++)
-                {
-                    float lensFlareScaleInterpolant = MathF.Cos(Utils.RandomFloat(ref seed) * MathHelper.TwoPi + Main.GlobalTimeWrappedHourly * 0.3f) * 0.5f + 0.5f;
-                    float lensFlareScale = MathF.Pow(spaceInterpolant, 0.7f) * MathHelper.Lerp(0.8f, 1.1f, lensFlareScaleInterpolant);
-                    Color lensFlareColor = Color.LightGoldenrodYellow;
-                    lensFlareColor.R -= 10;
-                    lensFlareColor.B += 12;
-                    lensFlareColor *= spaceInterpolant * lensFlareScale;
-                    lensFlareColor.A = 0;
-
-                    float lensFlareRotation = MathHelper.TwoPi * i / lensFlareCount + Main.GlobalTimeWrappedHourly * 0.045f;
-                    Main.spriteBatch.Draw(lensFlare, sunPosition, null, lensFlareColor, lensFlareRotation + lensFlareScaleInterpolant * 0.1f, lensFlare.Size() * 0.5f, lensFlareScale, 0, 0f);
-                }
             }
         }
 
